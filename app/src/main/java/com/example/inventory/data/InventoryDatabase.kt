c7a6198cbabadbc8732d8c7b9fa4a32b7aa81228
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2024-11-11 17:27:52 +0700

    inventorydb

--- app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -15,8 +15,7 @@ abstract class InventoryDatabase : RoomDatabase() {
         private var Instance: InventoryDatabase? = null
 
         fun getDatabase(context: Context): InventoryDatabase {
-            // if the Instance is not null, return it, otherwise create a new database instance.
-            return Instance ?: synchronized(this) {
+             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
                     .build().also { Instance = it }
             }
